e4144061e5e0644886e2bc4020134d8a47379de7
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-05-20 21:31:36 -0300

    Ajusta comentario do breakpoint test

--- tests/breakpoint-test.js
+++ tests/breakpoint-test.js
@@ -4,16 +4,13 @@ import { BASE_URL } from '../config.js'
 
 
 export const options = {
-    // Key configurations for Stress in this section
     executor: 'rampin-arrival-rate',
     stages: [
-        { duration: '1h', target: 50000 }, // rump-up de 1 até 50000 virtual users em 1 hora
+        { duration: '1h', target: 50000 }, // ramp-up até 50000 iterações por segundo em 1 hora 
     ],
 };
 
 export default function () {
-
-
     let response = http.get(`${BASE_URL}/api/books?category=Fantasia&available=true`)
     sleep(0.5)
 
